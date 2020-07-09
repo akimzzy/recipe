60352bb2267c1cabd437a9d3e7547b63d87436d8
akimzzy <a.akeem@yahoo.com>
2020-07-09 09:25:21 +0100

    Updates

---
+++ precache-manifest.8bc85db051cba33376b075ce50abd73b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e59a020bc36155fe66694b1de3b622",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "638a8bf999fea257a6c8",
+    "url": "/recipe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70bc0443bb549e04fed1",
+    "url": "/recipe/static/js/2.ffb7c972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe/static/js/2.ffb7c972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638a8bf999fea257a6c8",
+    "url": "/recipe/static/js/main.f0bfa6c5.chunk.js"
+  },
+  {
+    "revision": "f4f71c76c28a4db6d701",
+    "url": "/recipe/static/js/runtime-main.43a4647f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
